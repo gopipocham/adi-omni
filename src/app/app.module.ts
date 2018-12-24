0b31f086f6c5a45704d5b39cf6511ff63d80a2df
mounu12 <kanugantimamatha@gmail.com>
2018-12-24 17:28:39 +0530

    common ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MaterialModule } from './material/material.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common-ui/header/header.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     MaterialModule,
+    AppRoutingModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule
   ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+],
   providers: [],
   bootstrap: [AppComponent]
 })
