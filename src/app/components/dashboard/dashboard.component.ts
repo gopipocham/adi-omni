1c5bfce3e70424d4d55b7a97a1abcc0037fe1021
mounu12 <kanugantimamatha@gmail.com>
2018-12-27 18:26:26 +0530

    spinner added

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Output } from '@angular/core';
 import { TestService } from 'src/app/services/test.service';
 import {ConfimationDialogComponent} from 'src/app/shared/confimation-dialog/confimation-dialog.component'
 import { MatDialog } from '@angular/material';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,16 +11,18 @@ import { MatDialog } from '@angular/material';
 })
 export class DashboardComponent implements OnInit {
   nodedata: any[];
-  constructor(private testService : TestService, public dialog: MatDialog) { }
+  constructor(private testService : TestService, public dialog: MatDialog, private spinner: NgxSpinnerService) { }
   ngOnInit() {
   }
   postNodeValue(testnodes): void{
+    this.spinner.show();
     this.testService.postTestValues(testnodes).subscribe((data: any) => {
       this.nodedata= data.results;
       console.log(this.nodedata);
       if(data.success === true) {
+          this.spinner.hide();
         const dialogRef = this.dialog.open( ConfimationDialogComponent , {
-          width: '300px',
+          width: '700px',
         });
       }
     });
